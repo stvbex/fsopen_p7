6fb1b61d71b7af3ea3e8f80b65991263fe3ecbcc
stvbex <12851562+stvbex@users.noreply.github.com>
2020-12-24 16:31:26 +0200

    7.8

---
+++ fsopen_p7_country_hook/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    async function fetchCountry() {
+      try {
+        const res = await axios.get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+
+        const newCountry = {
+          found: true,
+          data: res.data[0]
+        }
+
+        setCountry(newCountry)
+      } 
+      catch (error) {
+        console.log(`country ${name} not found`)
+      }
+
+    }
+    fetchCountry()
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
